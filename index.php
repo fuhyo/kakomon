70b4daa31f14787c257ce051b2282f45c4a38833
toshi <toshi@mba132012.local>
2017-11-02 18:33:04 +0900

    add new file

--- index.php
+++ index.php
@@ -15,22 +15,50 @@ git commit
 
 
 */
- class Kakomon
- {
+
+/*
+
+ 出題モードを設定して、実行すると過去問が1題づつ出題される
+ 
+ 選べる項目
+ ・難易度
+ ・分野
+ ・年度
+ ・出題問題数
+ ・形式（4択、ox)
+ ・時間制限あり、なし
+
+
+*/
+
+// インスタンスを作成
+$kakomon_obj_a = new kakomon;
+
+// getter/setter
+$kakomon_obj_a->setYear("2015");
+echo $kakomon_obj_a->getYear();
+
+
+class Kakomon
+{
 	 //
 	 private $year;
-	 private $hoge;
+	 
+	 //
+	 public function __construct(){
+		 // インスタンス生成時に自動的にコール
+		 // デフォルトを設定する
+	 }
+	 
+	 // getter/setter
 	 public function setYear($year){
 		 $this->year = (string)filter_var($year);
 	 }
 	 public function getYear($year){
 		 return $this->year;
 	 }
- }
- 
- $kakomon_obj_a = new kakomon;
- $kakomon_obj_a->setYear("2015");
- 
- echo $kakomon_obj_a->getYear();
- 
+}
+// /class kakomon
+
+
 ?>
